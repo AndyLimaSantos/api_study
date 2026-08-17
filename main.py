99b7742be032f1602d0e7f8aee6ba16b42dfb5b8
AndyLimaSantos <anderson210798@gmail.com>
2026-08-16 23:00:31 -0300

    Correção nos commits

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 import pprint
+import os
+from dotenv import load_dotenv
+load_dotenv()
 #Vamos utilizar uma API de clima que podemos extrair informações do clima
-API_KEY = "418ae405459142a9ad9163118261608" #Não deixar aqui essa chave
+API_KEY_WEATHER = os.getenv("API_KEY_WEATHER") #Não deixar aqui essa chave
 link_api = "http://api.weatherapi.com/v1/current.json" #O link base para fazer extração de dados éhttp://api.weatherapi.com/v1
 
 
 #Na documentação da API é possivel ver oque é necessário passar para se fazer as requisições
 #ler a documentação é de extrema importância para que possamos não cometer erro.
-parametros = {'key':API_KEY,'q':'São Paulo','lang':'pt'}
+parametros = {'key':API_KEY_WEATHER ,'q':'São Paulo','lang':'pt'}
 resposta = requests.get(link_api, params = parametros) #O .get faz uma requisição, ou seja ele pucha um dado do link informado.
 
 #print(resposta.status_code)
